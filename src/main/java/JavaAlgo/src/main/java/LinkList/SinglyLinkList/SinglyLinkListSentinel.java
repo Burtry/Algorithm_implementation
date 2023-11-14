183485eba485ec9f190f185bec41ed71244b26f4
Burtry <1971797331@qq.com>
2023-11-14 12:12:16 +0800

    update code-- + 带头结点的单链表实现

--- src/main/java/JavaAlgo/src/main/java/LinkList/SinglyLinkList/SinglyLinkListSentinel.java
+++ src/main/java/JavaAlgo/src/main/java/LinkList/SinglyLinkList/SinglyLinkListSentinel.java
@@ -4,10 +4,10 @@ import java.util.Iterator;
 import java.util.function.Consumer;
 
 /**
- * 单向链表
+ * 单向链表带头结点（带哨兵）
  */
-public class SinglyLinkList implements Iterable<Integer>{
-    Node head;  //头指针
+public class SinglyLinkListSentinel implements Iterable<Integer>{
+    Node head = new Node(6,null);  //头节点
     /**
      * 节点类
      */
@@ -20,34 +20,39 @@ public class SinglyLinkList implements Iterable<Integer>{
             this.next = next;
         }
     }
+
+    //头插
     public void addFirst(int value) {
         /*//1链表为空
         head = new Node(value,null); */   //将头指针指向新节点(将新节点的引用值赋值给head)
         //2.链表非空
-        head = new Node(value,head);
+        /*head = new Node(value,head);*/
+        //带头结点可以直接调用insert()方法直接在索引位置为0的地方添加
+        insert(0,value);
+
     }
     /**
      * 遍历链表
      * @param consumer
      */
     public void loop1(Consumer<Integer> consumer) {
-        Node p = head;
+        Node p = head.next;
         while (p != null) {
-            //对于链表的操作最好由参数传入尽进来,具体要怎么用，由调用放进行决定
+            //对于链表的操作最好由参数传入尽进来,具体要怎么用，由调用方进行决定
             consumer.accept(p.value);
             p = p.next;
         }
     }
     //for循环
     public void loop2(Consumer<Integer> consumer) {
-        for(Node p = head; p != null;p = p.next){
+        for(Node p = head.next; p != null;p = p.next){
             consumer.accept(p.value);
         }
     }
     //迭代器遍历增强for循环 重写迭代器对象
     public Iterator<Integer> iterator() {
         return new Iterator<Integer>() {
-            Node p = head;
+            Node p = head.next;
             @Override
             public boolean hasNext() {  //询问是否有下一个元素
                 return p != null;
@@ -69,17 +74,23 @@ public class SinglyLinkList implements Iterable<Integer>{
     //向最后的节点后添加节点
     public void addLast(int value) {
         Node last = findLast();
-        if(last ==null) {
+        ///1.带头结点不需要进行判断
+
+ /*       if(last ==null) {
             addFirst(value);
             return;
-        }
+        }*/
+
         last.next = new Node(value,null);
     }
     //查找最后一个节点
     private Node findLast(){
-        if(head ==null){//空链表
+        ///2.带头结点不需要进行判断
+
+        /*if(head ==null){//空链表
             return null;
-        }
+        }*/
+
         Node p = head;
         while (p.next != null){
             p = p.next;
@@ -99,7 +110,7 @@ public class SinglyLinkList implements Iterable<Integer>{
     }
     //根据索引返回索引处的节点
     private Node findNode(int index) {
-        int i = 0;
+        int i = -1;     //当索引为0时，刚好指向头结点
         for (Node p = head;p !=null;p = p.next,i++){
             if(i ==index) return p;
         }
@@ -112,26 +123,34 @@ public class SinglyLinkList implements Iterable<Integer>{
      * @throws IllegalArgumentException 找不到抛出index异常
      */
     public void insert(int index, int value) throws IllegalArgumentException{
-        if(index ==0){
+        ///3.带头结点不需要进行判断
+
+        /*if(index ==0){
             addFirst(value);
             return;
-        }
+        }*/
 
-        Node node = findNode(index - 1);//找到上一个节点
-        if(node ==null) {//找不到
+        Node prev = findNode(index - 1);//找到上一个节点
+
+        if(prev ==null) {//找不到
             throw new IllegalArgumentException(String.format("index[%d] 不合法%n",index));
         }
-        node.next = new Node(value,node.next);
+        prev.next = new Node(value,prev.next);
     }
     /**
      * 删除第一个节点
      * @throws IllegalArgumentException 如果不存在，抛出IllegalArgumentException异常
      */
     public void removeFirst() throws IllegalArgumentException{
-        if(head.next == null){
+
+        //可以直接调用remove(0)来删除第一个节点
+       /* if(head.next == null){
             throw new IllegalArgumentException();
         }
-        head = head.next;
+        head = head.next;*/
+        remove(0);
+
+
     }
     /**
      * 删除索引位置的节点
@@ -139,9 +158,11 @@ public class SinglyLinkList implements Iterable<Integer>{
      * @throws IllegalArgumentException 找不到抛出异常
      */
     public void remove(int index) throws IllegalArgumentException{
-        if(index ==0){
+
+        ///4.带头结点不需要进行判断
+/*        if(index ==0){
             removeFirst();
-        }
+        }*/
 
         Node prev = findNode(index - 1);
         if(prev ==null){
--- src/main/java/JavaAlgo/src/main/java/LinkList/SinglyLinkList/TextSinglyLinkList/TextSinglyLinkList.java
+++ src/main/java/JavaAlgo/src/main/java/LinkList/SinglyLinkList/TextSinglyLinkList/TextSinglyLinkList.java
@@ -1,12 +1,25 @@
 package JavaAlgo.src.main.java.LinkList.SinglyLinkList.TextSinglyLinkList;
 
 import JavaAlgo.src.main.java.LinkList.SinglyLinkList.SinglyLinkList;
-import org.junit.jupiter.api.Assertions;
+import JavaAlgo.src.main.java.LinkList.SinglyLinkList.SinglyLinkListSentinel;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TextSinglyLinkList {
+
+    private static SinglyLinkListSentinel getSinglyListSentinel(){
+        SinglyLinkListSentinel singlyLinkList = new SinglyLinkListSentinel();
+        singlyLinkList.addLast(1);
+        singlyLinkList.addLast(2);
+        singlyLinkList.addLast(3);
+        singlyLinkList.addLast(4);
+        return singlyLinkList;
+    }
+
 
     @Test
     public void text1() {
@@ -22,6 +35,7 @@ public class TextSinglyLinkList {
         singlyLinkList.loop2(value-> {
             System.out.println(value);
         });
+
     }
 
     @Test
@@ -46,6 +60,41 @@ public class TextSinglyLinkList {
         for (Integer i : singlyLinkList) {
             System.out.println(i);
         }
+
+    }
+
+    @Test
+    @DisplayName("测试insert")
+    public void text4(){
+        SinglyLinkListSentinel list = getSinglyListSentinel();
+        list.insert(0,5);
+        assertIterableEquals(List.of(5,1,2,3,4),list);
+
+        list = getSinglyListSentinel();
+        list.addFirst(5);
+        assertIterableEquals(List.of(5,1,2,3,4),list);
+
+        SinglyLinkListSentinel linkListSentinel = new SinglyLinkListSentinel();
+        linkListSentinel.addFirst(1);
+        assertIterableEquals(List.of(1),linkListSentinel);
+    }
+
+    @Test
+    @DisplayName("测试remove")
+    public void text5(){
+        SinglyLinkListSentinel singlyListSentinel = getSinglyListSentinel();
+        singlyListSentinel.remove(2);
+        assertIterableEquals(List.of(1,2,4),singlyListSentinel);
+        singlyListSentinel.remove(0);
+        assertIterableEquals(List.of(2,4),singlyListSentinel);
+        singlyListSentinel.removeFirst();
+        assertIterableEquals(List.of(4),singlyListSentinel);
+        singlyListSentinel.removeFirst();
+        assertIterableEquals(List.of(),singlyListSentinel);
+        assertThrows(IllegalArgumentException.class,singlyListSentinel::removeFirst);
+
+        assertThrows(IllegalArgumentException.class,()->singlyListSentinel.remove(5));
+        assertThrows(IllegalArgumentException.class,()->singlyListSentinel.remove(7));
 
     }
 }
