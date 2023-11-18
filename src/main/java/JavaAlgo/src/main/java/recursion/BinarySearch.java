11caf7693e9f5cd5e5cd6a1779118c8943515e0c
Burtry <1971797331@qq.com>
2023-11-18 10:27:35 +0800

    递归 01 - 04

---
+++ src/main/java/JavaAlgo/src/main/java/recursion/BinarySearch.java
@@ -0,0 +1,39 @@
+package JavaAlgo.src.main.java.recursion;
+
+/**
+ * 递归二分查找
+ */
+public class BinarySearch {
+
+    //对外只需要数组和目标值
+    public static int search(int[] a, int target) {
+        return fun(a,target,0,a.length -1);
+    }
+
+
+
+
+    /**
+     * 递归函数
+     * @param a 待查找的数组
+     * @param target 待查找的目标朱
+     * @param i 起始点
+     * @param j 结束点
+     * @return 返回值
+     */
+    private static int fun(int[] a, int target, int i, int j) {
+
+        //递归结束条件
+        if (i > j) {
+            return -1;
+        }
+        int m = (i + j) >>> 1;
+        if(target < a[m]) {
+            return fun(a, target, i, m -1);
+        } else if (a[m] < target) {
+            return fun(a,target,m +1, j);
+        } else {
+            return m;
+        }
+    }
+}
---
+++ src/main/java/JavaAlgo/src/main/java/recursion/BubbleSort.java
@@ -0,0 +1,41 @@
+package JavaAlgo.src.main.java.recursion;
+
+/**
+ * 递归冒泡排序
+ * 将数组划分成两部分[0...j] [j+1.....a.length-1]
+ * 左边[0j]是未排序部分
+ * 右边j+1.a.length-1]是已排序部分
+ * 未排序区间内，相邻的两个元素比较，如果前一个大于后一个，则交换位置
+ */
+public class BubbleSort {
+
+    public static void sort(int[] arr) {
+        fun(arr, arr.length - 1);
+
+    }
+
+
+    /**
+     * 冒泡排序的交换逻辑
+     * @param arr 待排序的数组
+     * @param j 未排序的右边界
+     */
+    private static void fun(int[] arr, int j) {
+
+        //递归结束条件
+        if (j == 0) {
+            return;
+        }
+
+        for (int i = 0; i < j; i++) {
+            if (arr[i] > arr[i + 1]) {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+        }
+        //递归调用
+        fun(arr, j-1);
+    }
+
+}
