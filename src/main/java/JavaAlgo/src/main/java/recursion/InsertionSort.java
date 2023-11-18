05a2279d29bf826fd0723a39d8449979775039e8
Burtry <1971797331@qq.com>
2023-11-18 11:08:54 +0800

    递归 插入排序优化

--- src/main/java/JavaAlgo/src/main/java/recursion/InsertionSort.java
+++ src/main/java/JavaAlgo/src/main/java/recursion/InsertionSort.java
@@ -10,6 +10,10 @@ public class InsertionSort {
 
     }
 
+    public static void sort2(int[] arr) {
+        insertion2(arr,1, arr.length - 1);
+    }
+
 
     /**
      *
@@ -33,6 +37,25 @@ public class InsertionSort {
         arr[i + 1] = tmp;
 
         insertion(arr, low +1);
+    }
+
+    private static void insertion2(int[] arr, int low, int high) {
+        if(low == high) {
+            return;
+        }
+
+        int tmp = arr[low];
+        int i = low -1; //已排序区域指针
+
+        //找比tmp小的元素
+        while (i >= 0 && arr[i] > tmp) {
+            arr[i + 1] = arr[i];    //空出插入位置
+            i--;
+        }
+        //找到了
+        arr[i + 1] = tmp;
+
+        insertion2(arr, low +1, high);
     }
 
 }
--- src/main/java/JavaAlgo/src/main/java/recursion/text/TestE05InsertionSort.java
+++ src/main/java/JavaAlgo/src/main/java/recursion/text/TestE05InsertionSort.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static JavaAlgo.src.main.java.recursion.InsertionSort.sort;
+import static JavaAlgo.src.main.java.recursion.InsertionSort.sort2;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class TestE05InsertionSort {
@@ -27,5 +28,27 @@ public class TestE05InsertionSort {
         int[] a3 = {1, 2, 3, 4, 5};
         sort(a3);
         assertArrayEquals(expected, a3);
+    }
+
+
+    @Test
+    @DisplayName("测试递归插入")
+    public void test2() {
+        int[] expected = {2, 3, 4, 5, 1};
+        int[] a1 = {5, 4, 3, 2, 1};
+        sort2(a1);
+        assertArrayEquals(expected, a1);
+
+       /* int[] a2 = {4, 5, 3, 2, 1};
+        sort2(a2);
+        assertArrayEquals(expected, a2);
+
+        int[] a4 = {3, 1, 4, 2, 5};
+        sort2(a4);
+        assertArrayEquals(expected, a4);
+
+        int[] a3 = {1, 2, 3, 4, 5};
+        sort2(a3);
+        assertArrayEquals(expected, a3);*/
     }
 }
