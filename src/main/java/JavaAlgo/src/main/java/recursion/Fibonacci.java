701c9db5fce5ddb1412a0ae96794ad6b10d2e6ee
Burtry <1971797331@qq.com>
2023-11-21 10:32:54 +0800

    递归 斐波那契数列初实现

---
+++ src/main/java/JavaAlgo/src/main/java/recursion/Fibonacci.java
@@ -0,0 +1,23 @@
+package JavaAlgo.src.main.java.recursion;
+
+/**
+ * 递归实现斐波那契数列
+ */
+public class Fibonacci {
+
+    public static int fun(int n) {
+
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        int x = fun(n - 1);
+        int y = fun(n - 2);
+        return x + y;
+    }
+    public static void main(String[] args) {
+        System.out.println(fun(8));
+    }
+}
