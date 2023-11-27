b1c7c1cce760c10cbe0650e47b52e4e2919dd40b
Burtry <1971797331@qq.com>
2023-11-27 09:40:13 +0800

    斐波那契数列优化 + 兔子问题

--- src/main/java/JavaAlgo/src/main/java/recursion/RabbitProblem.java
+++ src/main/java/JavaAlgo/src/main/java/recursion/RabbitProblem.java
@@ -1,21 +1,15 @@
 package JavaAlgo.src.main.java.recursion;
 
 /**
- * 递归实现斐波那契数列
- *
+ * 兔子问题
  */
-
-/**
- * 斐波那契数列的递归次数： 2* fun(n+1) -1
- */
-public class Fibonacci {
-
+public class RabbitProblem {
     public static int fun(int n) {
 
-        if (n == 0) {
-            return 0;
-        }
         if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
             return 1;
         }
         int x = fun(n - 1);
@@ -23,7 +17,6 @@ public class Fibonacci {
         return x + y;
     }
     public static void main(String[] args) {
-        System.out.println(fun(8));
+        System.out.println(fun(6));
     }
-
 }
