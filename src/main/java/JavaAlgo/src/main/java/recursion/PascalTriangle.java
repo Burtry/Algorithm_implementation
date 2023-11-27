fa33e0c163ca0b77b3011825048a1f50c9f76543
Burtry <1971797331@qq.com>
2023-11-27 15:40:37 +0800

    杨辉三角

---
+++ src/main/java/JavaAlgo/src/main/java/recursion/PascalTriangle.java
@@ -0,0 +1,36 @@
+package JavaAlgo.src.main.java.recursion;
+
+/**
+ * 杨辉三角
+ */
+public class PascalTriangle {
+
+    private static int element(int i, int j) {
+        if (j == 0 || j == i) {
+            return  1;
+        }
+        return element(i - 1,j - 1) + element(i - 1, j);
+    }
+
+    private static void printSpace(int n, int i) {
+        int num = (n - 1 - i) * 2;
+
+        for (int j = 0; j < num; j++) {
+            System.out.print(" ");
+        }
+    }
+
+    public static void print(int n) {
+        for (int i = 0; i < n; i++) {
+            printSpace(n,i);
+            for (int j = 0; j <= i; j++) {
+                System.out.printf("%-4d",element(i,j));
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        print(5);
+    }
+}
